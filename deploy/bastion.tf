f1cef70bf0595cd8a9ad869c7a573b2f71422990
Misha Melnyk <as.k.github@melnyk.com>
2023-07-17 08:26:08 -0400

    Add ECS setup
    Adds the setup for ECS to run a task with the application image

---
+++ deploy/bastion.tf
@@ -0,0 +1,61 @@
+# Small EC2 instance to run the setup script and to work as a bastion host
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  
+  key_name      = var.bastion_key_pair
+  subnet_id = aws_subnet.public_sub[1].id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  associate_public_ip_address = true
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+  tags = {
+    Name = "bastion"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+
+
+ filter {
+   name   = "name"
+   values = ["al2023-ami*"]
+ }
+}
+
+resource "aws_iam_role" "bastion_role" {
+  name = "${var.application_name}-bastionRole"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "bastion-image-pull" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = aws_iam_policy.image_pull.arn
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.application_name}-bastion"
+  role = aws_iam_role.bastion_role.name
+}
--- deploy/resources.tf
+++ deploy/resources.tf
@@ -1,62 +1,14 @@
-# Small EC2 instance to run the setup script and to work as a bastion host
-resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  
-  key_name      = var.bastion_key_pair
-  subnet_id = aws_subnet.public_sub[1].id
-  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-  associate_public_ip_address = true
 
-  tags = {
-    Name = "bastion"
-  }
-}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-# User Database - Postgres
-resource "aws_db_instance" "user_db" {
-  identifier             = "user-db"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "15.3"
-  db_name                = "user_db"
-  username               = "cs446"
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.user_db.name
-  vpc_security_group_ids = [aws_security_group.user_db.id]
-  parameter_group_name   = aws_db_parameter_group.user_db.name
-  publicly_accessible    = false
-  skip_final_snapshot    = true
-}
-
-resource "aws_db_parameter_group" "user_db" {
-  name   = "user-db"
-  family = "postgres15"
-
-  parameter {
-    name  = "log_connections"
-    value = "1"
-  }
-}
 
 
 # Vespa ECS
 
-# gRPC Image
+# Springboot ECS
+
+
+
+#################
+# Load Balancer #
+#################
