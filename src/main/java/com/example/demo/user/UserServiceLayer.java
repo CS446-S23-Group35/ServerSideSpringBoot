bc12cca352801eb0b139102e213dee52505dd693
Ahmed Mushtaha <amushtaha@uwaterloo.ca>
2023-07-18 14:10:27 -0400

    Implemented some API methods for inventory/users tables

--- src/main/java/com/example/demo/user/UserServiceLayer.java
+++ src/main/java/com/example/demo/user/UserServiceLayer.java
@@ -4,7 +4,8 @@ package com.example.demo.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -12,23 +13,29 @@ public class UserServiceLayer {
 
     @Autowired
     UserDBLayer userDBLayer;
-    public User getUser(String userName){
+
+    @Autowired
+    InventoryDBLayer inventoryDBLayer;
+
+    public User getUser(String userName) {
         return userDBLayer.getUser(userName);
     }
 
-    public List<FoodItem> getUsersInventory(String id) {
+    public HashMap<Long, FoodItem> getUsersInventory(String id) {
         return userDBLayer.getUsersInventory(id);
-
     }
+
     public FoodItem getFoodItemForUser(String id, String foodItem) {
         return userDBLayer.getFoodItemForUser(id, foodItem);
     }
 
-    public Boolean addItemToUserInventory(String id, String foodItem) {
-        return userDBLayer.addItemToUserInventory(id, foodItem);
+    public Boolean addItemToUserInventory(String id, FoodItem foodItem) {
+        if (userDBLayer.getUser(id) == null)
+            return false;
+        return inventoryDBLayer.addItemForUser(id, foodItem) && userDBLayer.addItemToUserInventory(id, foodItem);
     }
 
-    public Boolean deleteItemFromUserInventory(String id, String foodItem) {
+    public Boolean deleteItemFromUserInventory(String id, Long foodItem) {
         return userDBLayer.deleteItemFromUserInventory(id, foodItem);
     }
 
@@ -36,22 +43,21 @@ public class UserServiceLayer {
         List<FoodItem> shoppingList = userDBLayer.getUserShoppingList(id);
         // ensure items in shopping list have an "NA" value of expiration date
         for (int i = 0; i < shoppingList.size(); i++) {
-            shoppingList.get(i).setTimeTillExpiration(-1);
+            shoppingList.get(i).setExpiryDate(new Date(0L));
         }
         return shoppingList;
     }
 
     public Boolean addItemToUserShoppingList(String id, String foodItem) {
-        return userDBLayer.addItemToUserShoppingList(id,foodItem);
+        return userDBLayer.addItemToUserShoppingList(id, foodItem);
     }
 
     public Boolean removeItemFromUserShoppingList(String id, String foodItem) {
-        return userDBLayer.removeItemFromUserShoppingList(id,foodItem);
+        return userDBLayer.removeItemFromUserShoppingList(id, foodItem);
     }
 
     public Boolean deleteUserShoppingList(String id) {
         return userDBLayer.deleteUserShoppingList(id);
     }
-
 
 }
