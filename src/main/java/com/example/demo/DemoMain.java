eee54b7b857207ab2a3c005e3aa50fb27e00c629
Misha Melnyk <as.k.github@melnyk.com>
2023-07-24 02:40:38 -0400

    opensearch config

--- src/main/java/com/example/demo/DemoMain.java
+++ src/main/java/com/example/demo/DemoMain.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.example.demo.recipe.OpenSearchImpl;
+import com.example.demo.recipe.Recipe;
+import com.example.demo.recipe.Searcher;
+
+// @SpringBootApplication
 public class DemoMain {
 
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(DemoMain.class, args);
+	// }
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoMain.class, args);
+		Searcher searcher = new OpenSearchImpl("localhost");
+		List<Recipe> recipes = searcher.SearchByName("test", null);
+		System.out.println("Found " + recipes.size() + " recipes");
+		for(Recipe recipe : recipes){ 
+			System.out.println(recipe.name);
+		}
 	}
 
 }
