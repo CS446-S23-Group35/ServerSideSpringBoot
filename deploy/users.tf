a9e5fd234d6eb5f15f8190166f69272c9e2f7924
Misha Melnyk <as.k.github@melnyk.com>
2023-07-24 21:55:29 -0400

    update password policy

--- deploy/users.tf
+++ deploy/users.tf
@@ -8,11 +8,14 @@ resource "aws_cognito_user_pool" "pool" {
     }
   }
 
+  auto_verified_attributes = [ "email" ]
+
   password_policy {
     minimum_length    = 8
     require_lowercase = true
     require_numbers   = true
     require_symbols   = true
     require_uppercase = true
+    temporary_password_validity_days = 7
   }
 }
