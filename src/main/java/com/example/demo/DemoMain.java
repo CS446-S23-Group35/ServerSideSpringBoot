e0d18945c110573fe86497c0723d6641342d711f
Misha Melnyk <as.k.github@melnyk.com>
2023-07-26 11:02:56 -0400

    Clean up main function, fix rebase error

--- src/main/java/com/example/demo/DemoMain.java
+++ src/main/java/com/example/demo/DemoMain.java
@@ -1,15 +1,7 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.demo.recipe.OpenSearchImpl;
-import com.example.demo.recipe.Recipe;
-import com.example.demo.recipe.Searcher;
 
 @SpringBootApplication
 public class DemoMain {
@@ -17,25 +9,4 @@ public class DemoMain {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMain.class, args);
 	}
-
-	// public static void main(String[] args) {
-	// 	Searcher.Filters filters =  Searcher.Filters.empty()
-	// 	.withExcludedIngredients(
-	// 		Arrays.asList("flour", "farro")
-	// 	).withInventoryIngredients(
-	// 		Arrays.asList("paprika", "vanilla extract", "coffee")
-	// 	).withExpiringIngredients(
-	// 		Arrays.asList("pork")
-	// 	);//.withDietaryRestrictions(
-	// 	// 	Arrays.asList(Searcher.DietaryRestrictions.VEGAN)
-	// 	// );
-
-	// 	Searcher searcher = new OpenSearchImpl("localhost");
-	// 	List<Recipe> recipes = searcher.SearchByInventory(filters);
-	// 	System.out.println("Found " + recipes.size() + " recipes");
-	// 	for(Recipe recipe : recipes){ 
-	// 		System.out.println(recipe.name);
-	// 	}
-	// }
-
 }
